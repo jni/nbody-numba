b885b2213390342f8391794600cb4fcfa86ecbef
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-08-11 15:20:23 +1000

    More work on numba compliance

--- nbody-numba.py
+++ nbody-numba.py
@@ -6,8 +6,10 @@
 # modified by Maciej Fijalkowski
 # 2to3
 
-import sys 
-import numba as nb
+import sys
+import numpy as np
+import numba
+
 
 def combinations(l):
     result = []
@@ -20,45 +22,46 @@ def combinations(l):
 PI = 3.14159265358979323
 SOLAR_MASS = 4 * PI * PI
 DAYS_PER_YEAR = 365.24
+BODIES = ['sun', 'jupiter', 'saturn', 'uranus', 'neptune']
+INDICES = dict(zip(BODIES, range(len(BODIES))))
 
-BODIES = {
-    'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+# An array describing the system.
+# First three columns [0:3] are positions in space.
+# Next three columns [3:6] are velocity.
+# Final columns [6] is the mass.
+SYSTEM = np.array([
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, SOLAR_MASS],
+    [4.84143144246472090e+00,
+                -1.16032004402742839e+00,
+                -1.03622044471123109e-01,
+                1.66007664274403694e-03 * DAYS_PER_YEAR,
+                7.69901118419740425e-03 * DAYS_PER_YEAR,
+                -6.90460016972063023e-05 * DAYS_PER_YEAR,
+                9.54791938424326609e-04 * SOLAR_MASS],
+    [8.34336671824457987e+00,
+               4.12479856412430479e+00,
+               -4.03523417114321381e-01,
+               -2.76742510726862411e-03 * DAYS_PER_YEAR,
+               4.99852801234917238e-03 * DAYS_PER_YEAR,
+               2.30417297573763929e-05 * DAYS_PER_YEAR,
+               2.85885980666130812e-04 * SOLAR_MASS],
+    [1.28943695621391310e+01,
+               -1.51111514016986312e+01,
+               -2.23307578892655734e-01,
+               2.96460137564761618e-03 * DAYS_PER_YEAR,
+               2.37847173959480950e-03 * DAYS_PER_YEAR,
+               -2.96589568540237556e-05 * DAYS_PER_YEAR,
+               4.36624404335156298e-05 * SOLAR_MASS],
+    [1.53796971148509165e+01,
+                -2.59193146099879641e+01,
+                1.79258772950371181e-01,
+                2.68067772490389322e-03 * DAYS_PER_YEAR,
+                1.62824170038242295e-03 * DAYS_PER_YEAR,
+                -9.51592254519715870e-05 * DAYS_PER_YEAR,
+                5.15138902046611451e-05 * SOLAR_MASS]]
 
-    'jupiter': ([4.84143144246472090e+00,
-                 -1.16032004402742839e+00,
-                 -1.03622044471123109e-01],
-                [1.66007664274403694e-03 * DAYS_PER_YEAR,
-                 7.69901118419740425e-03 * DAYS_PER_YEAR,
-                 -6.90460016972063023e-05 * DAYS_PER_YEAR],
-                9.54791938424326609e-04 * SOLAR_MASS),
 
-    'saturn': ([8.34336671824457987e+00,
-                4.12479856412430479e+00,
-                -4.03523417114321381e-01],
-               [-2.76742510726862411e-03 * DAYS_PER_YEAR,
-                4.99852801234917238e-03 * DAYS_PER_YEAR,
-                2.30417297573763929e-05 * DAYS_PER_YEAR],
-               2.85885980666130812e-04 * SOLAR_MASS),
-
-    'uranus': ([1.28943695621391310e+01,
-                -1.51111514016986312e+01,
-                -2.23307578892655734e-01],
-               [2.96460137564761618e-03 * DAYS_PER_YEAR,
-                2.37847173959480950e-03 * DAYS_PER_YEAR,
-                -2.96589568540237556e-05 * DAYS_PER_YEAR],
-               4.36624404335156298e-05 * SOLAR_MASS),
-
-    'neptune': ([1.53796971148509165e+01,
-                 -2.59193146099879641e+01,
-                 1.79258772950371181e-01],
-                [2.68067772490389322e-03 * DAYS_PER_YEAR,
-                 1.62824170038242295e-03 * DAYS_PER_YEAR,
-                 -9.51592254519715870e-05 * DAYS_PER_YEAR],
-                5.15138902046611451e-05 * SOLAR_MASS) }
-
-
-SYSTEM = list(BODIES.values())
-PAIRS = combinations(SYSTEM)
+PAIRS = np.array(combinations(range(len(BODIES))))
 
 
 @nb.jit(nopython=True)
